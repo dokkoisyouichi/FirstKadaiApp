564ad6015a1be8e014f02ba343b2536002f434dc
Keisuke Minami <Keisuke@keisukes-macbook-pro.local>
2019-03-07 00:20:36 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Keisuke Minami on 3/7/31 H.
+//  Copyright © 31 Heisei Keisuke Minami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
